f50444b26811deac00330ebf9b582acc5ace552f
“Michelle” <chelle.ov@email.com>
2025-12-01 21:58:32 +0200

    Disabled unimplemented device types

--- backend/src/sim_device_control/schemas.py
+++ backend/src/sim_device_control/schemas.py
@@ -11,10 +11,10 @@ class DeviceType(Enum):
     TEMPERATURE_SENSOR = "temperature_sensor"
     PRESSURE_SENSOR = "pressure_sensor"
     HUMIDITY_SENSOR = "humidity_sensor"
-    DC_MOTOR = "dc_motor"
-    STEPPER_MOTOR = "stepper_motor"
-    DIGITAL_PORT = "digital_port"
-    ANALOG_PORT = "analog_port"
+    # DC_MOTOR = "dc_motor"
+    # STEPPER_MOTOR = "stepper_motor"
+    # DIGITAL_PORT = "digital_port"
+    # ANALOG_PORT = "analog_port"
 
 
 class SimDevice(BaseModel):
