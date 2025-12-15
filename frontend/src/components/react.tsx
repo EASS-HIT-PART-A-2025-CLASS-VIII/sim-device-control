e10eeb098095cc9f6da593022966693f43718f50
“Michelle” <chelle.ov@email.com>
2025-12-15 19:59:31 +0200

    Added minor logic for general idea of frontend

---
+++ frontend/src/components/react.tsx
@@ -0,0 +1,11 @@
+import reactLogo from '../assets/react.svg'
+
+export default function ReactComponent() {
+  return (
+    <div>
+        <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+    </div>
+  );
+}
